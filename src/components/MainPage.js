4a64c86a26d6d319505b517dafc3b94a00dd5421
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-03-20 23:30:51 +0100

    fixed the api issue

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -6,7 +6,13 @@ import { useEffect, useState } from "react";
 
 const MainPage = () => {
     const [search, setSearch] = useState('');
-    const [shouldRun, setShouldRun] = useState(false)
+    // const [shouldRun, setShouldRun] = useState(false);
+    // const [input, setInput] = useState('');
+    // const [output, setOutput] = useState([]);
+    const [apiData, setApiData] = useState([]);
+
+    const [post, setPost] = useState(null);
+    let myObject;
 
 
     const options = {
@@ -17,57 +23,65 @@ const MainPage = () => {
         }
     };
 
-    // const url = `https://youtube-search-results.p.rapidapi.com/youtube-search/?q=${search}`
-    // useEffect(() => {
-    //     if (shouldRun) {
-    //         fetch(url, options)
-    //             .then(response =>
-    //                 response.json()
-    //             ).then(json => {
-    //                 console.log(json)
-    //             }).catch(e => {
-    //                 console.log("e", e)
-    //             })
-    //     }
-    // }, [])
+    // const getData = () => {
+    //     fetch(`https://youtube-search-results.p.rapidapi.com/youtube-search/?q=${search}`, options)
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             // console.log(data);
+    //             setOutput(data);
+    //             // myObject = JSON.parse(data)
+    //             console.log(output.items)
+    //             setSearch('');
+    //         })
+    //         .catch(error => console.error(error));
+    // };
 
     const getData = async () => {
-        const res = await fetch(`https://youtube-search-results.p.rapidapi.com/youtube-search/?q=${search}`, options);
-        const data = await res.json();
-        // setUsers(data);
-        console.log(data)
+        const response = await fetch(`https://youtube-search-results.p.rapidapi.com/youtube-search/?q=${search}`, options);
+        const data = await response.json();
+        setApiData(data.items);
+        console.log(data.items)
+        // console.log(apiData)
     };
 
-
+    const updateInput = (event) => {
+        setSearch(event.target.value)
+    }
 
     const test = (e) => {
         e.preventDefault();
-        setShouldRun(true);
+
         getData();
-        // console.log(search)
-        // fetchData(search)
-        // setShouldRun(false);
+
+        // setSearch('');
+
 
     }
-    const updateInput = (event) => {
-        // console.log(event.target.value);
-        setSearch(event.target.value)
-    }
+
 
     return (
 
         <div className="w-[100vw] h-[100vh] bg-slate-900 flex-col justify-center items-center flex ">
             <div>
-                <div className="flex justify-center">
-                    <h1 className="text-white">You<span className="text-red-600">Search</span></h1>
+                <div className="flex justify-center mb-4">
+                    <h1 className="text-white text-3xl">You<span className="text-red-600">Search</span></h1>
                 </div>
 
                 <form onSubmit={test}>
                     <div className="w-fit rounded-lg">
-                        <input className="h-10 rounded-l-lg pl-4 w-[40vw]" type="text" name="id" onChange={updateInput} />
+                        <input className="h-10 rounded-l-lg pl-4 lg:w-[20vw] w-[40vw]" type="text" name="id" value={search} onChange={updateInput} />
                         <button className="bg-red-700 hover:bg-red-900 text-white font-bold py-2 px-4 rounded-r-lg">Search</button>
                     </div>
                 </form>
+                <div>
+                    {apiData?.map((item) => (
+                        <div>
+                            <p>{item.name ? item.name : item.title}</p>
+                            {/* <img src={item?.bestThumbnail.url} alt={item.title} /> */}
+                            {/* <p>{item?.author.avatars.url}</p> */}
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
